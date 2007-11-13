3e25eeb6a4654e0c14749c5392b836f186f836bb
cinu <cinu>
2007-11-13 19:53:09 +0000

    poprawienie gg_login_hash

--- Helpers.py
+++ Helpers.py
@@ -11,24 +11,36 @@ class Helpers(object):
 	hash_type = 0x01 # -- hash obliczany za pomoca "zwyklej", "starej" funkcji do obliczania gg_hashu
 	#hash_type = 0x02 # -- hash obliczany za pomoca sha-1
 	def gg_login_hash(password, seed):
-		#assert type(password) == types.StringType
-		#assert type(seed) == types.IntType
+		assert type(password) == types.StringType
+		assert type(seed) == types.IntType
 		x = 0L
-		y = seed
+		y = long(seed)
 		z = 0L
 		for c in password:
-			x = (x and 0xffffff00) or c
+			x = (x & 0xffffff00L) | ord(c)
 			y ^= x
+			y &= 0xffffff00L
 			y += x
+			y &= 0xffffff00L
 			x <<= 8
+			x &= 0xffffff00L
 			y ^= x
+			y &= 0xffffff00L
 			x <<= 8
+			x &= 0xffffff00L
 			y -= x
+			y &= 0xffffff00L
 			x <<= 8
+			x &= 0xffffff00L
 			y ^= x
-			z = y and 0x1f
-			y = (y << z) or (y >> (32 - z))
+			y &= 0xffffff00L
+			z = y & 0x1f
+			y = (y << z) | (y >> (32 - z))
+			y &= 0xffffff00L
 		return y
+
 	def ip_string_to_int32(ip):
 		#assert type(ip) == types.StringType
 		return NotImplemented
+	
+	gg_login_hash = staticmethod(gg_login_hash)
