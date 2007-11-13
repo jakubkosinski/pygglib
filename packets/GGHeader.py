4917b68a13916f422155d7920170b5f25312b5fd
cinu <cinu>
2007-11-13 23:55:07 +0000

    header - poprawka

--- packets/GGHeader.py
+++ packets/GGHeader.py
@@ -13,17 +13,28 @@ class GGHeader(object):
 	Kazdy pakiet wysylany/pobierany do/od serwera zawiera na poczatku
 	naglowek - tym naglowkiem jest wlasnie struktura GGHeader.
 	"""
-	def __init__(self, type_, length, connection = None):
+
+	def __init__(self):
+		"""
+		Konstruktor ten wywolujemy gdy naglowek bedziemy odbierac
+		"""
+		pass
+	
+	def __init__(self, type_, length):
+		"""
+		Konstruktor ten wywolujemy wtedy kiedy pakiet jest przeznaczony do wyslania
+		"""
 		assert type(type_) == types.IntType
 		assert type(length) == types.IntType
-		assert connection == None or type(connection) == Connection
 		
 		self.type = type_
 		self.length = length
+		self.connection = None
 	
-	def read(self):
-		self.type = connection.read_int32()
-		self.length = connection.read_int32()
+	def read(self, connection):
+		assert type(connection) == Connection
+		data = connection.read()
+		self.type, self.length = struct.unpack("<II", data)
 	
 	def __repr__(self):
 		return struct.pack("<II", self.type, self.length)
