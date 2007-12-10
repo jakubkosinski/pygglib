18709b1c4350f280449af84aaa847087902e540c
ghandal <ghandal>
2007-12-10 18:24:41 +0000

    Dodano mozliwosc przypominania hasla na adres email

--- HTTPServices.py
+++ HTTPServices.py
@@ -80,14 +80,14 @@ class HTTPServices(object):
 		return Token(width, height, length, id, url + '?tokenid=' + id)
 
 	def register_account(self, pwd, email, tokenid, tokenval):
-	"""
-	Metoda rejestruje nowe konto w sieci Gadu-Gadu. Przyjmuje nastepujace parametry:
-		* pwd - haslo uzytkownika
-		* email - email uzytkownika, sluzacy do odzyskiwania hasla
-		* tokenid - ID tokena pobranego metoda get_token_data
-		* tokenval - tresc z obrazka tokena
-	Zwraca przydzielony numer konta. W przypadku blednych danych tokena rzucany jest wyjatek
-	"""
+		"""
+		Metoda rejestruje nowe konto w sieci Gadu-Gadu. Przyjmuje nastepujace parametry:
+			* pwd - haslo uzytkownika
+			* email - email uzytkownika, sluzacy do odzyskiwania hasla
+			* tokenid - ID tokena pobranego metoda get_token_data
+			* tokenval - tresc z obrazka tokena
+		Zwraca przydzielony numer konta. W przypadku blednych danych tokena rzucany jest wyjatek
+		"""
 		code = Helpers.gg_http_hash(email, pwd)
 		url = 'http://register.gadu-gadu.pl/appsvc/fmregister3.asp'
 		user_agent = 'Mozilla/4.0 (compatible; MSIE 5.0; Windows 98)'
@@ -98,21 +98,20 @@ class HTTPServices(object):
 		
 		response = urllib2.urlopen(request)
 		text = response.read()
-		print text
 		if text == 'bad_tokenval':
 			raise Exception('HTTPServices.register_account: Bad tokenval')
 		uin = text[text.find(':')+1:len(text)]
 		return uin
 		
 	def delete_account(self, fmnumber, fmpwd, tokenid, tokenval):
-	"""
-	Metoda usuwa konto o numerze fmnumber z serwerow Gadu-Gadu. Przyjmuje nastepujace parametry:
-		* fmnumber - numer Gadu-Gadu do usuniecia
-		* fmpwd - haslo do numeru
-		* tokenid - ID tokena pobranego metoda get_token_data
-		* tokenval - tresc z obrazka tokena
-	Zwraca True gdy numer zostanie usuniety lub False w przeciwnym wypadku
-	"""
+		"""
+		Metoda usuwa konto o numerze fmnumber z serwerow Gadu-Gadu. Przyjmuje nastepujace parametry:
+			* fmnumber - numer Gadu-Gadu do usuniecia
+			* fmpwd - haslo do numeru
+			* tokenid - ID tokena pobranego metoda get_token_data
+			* tokenval - tresc z obrazka tokena
+		Zwraca True gdy numer zostanie usuniety lub False w przeciwnym wypadku
+		"""
 		code = Helpers.gg_http_hash('deleteaccount@gadu-gadu.pl', fmpwd)
 		random.seed(int(time.time()))
 		pwd = random.randint(0,0xffff)
@@ -130,7 +129,32 @@ class HTTPServices(object):
 		else:
 			return False
 	
+	def remind_password(self, userid, email, tokenid, tokenval):
+		"""
+		Metoda wysyla przypomnienie hasla na adres email podany przy rejestracji. Przyjmuje parametry:
+			* userid - numer Gadu-Gadu uzytkownika
+			* email - email podany przy rejestracji
+			* tokenid - ID tokena pobranego metoda get_token_data
+			* tokenval - tresc z obrazka tokena
+		Zwraca True gdy przypomnienie zostalo wyslane, False w przeciwnym wypadku
+		"""
+		code = Helpers.gg_http_hash(str(userid), email)
+		url = 'http://retr.gadu-gadu.pl/appsvc/fmsendpwd3.asp'
+		user_agent = 'Mozilla/4.0 (compatible; MSIE 5.0; Windows 98)'
+		data = urllib.urlencode({'userid' : userid, 'email' : email, 'tokenid' : tokenid, 'tokenval' : tokenval, 'code' : code})
+		
+		request = urllib2.Request(url, data)
+		request.add_header('User-Agent', user_agent)
+		
+		response = urllib2.urlopen(request)
+		text = response.read()
+		if text == 'pwdsend_success':
+			return True
+		else:
+			return False
+		
 	get_server = classmethod(get_server)
 	get_token_data = classmethod(get_token_data)
 	register_account = classmethod(register_account)
 	delete_account = classmethod(delete_account)
+	remind_password = classmethod(remind_password)
