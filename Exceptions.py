f715e86482072bee392bb570421832ca16310150
bse <bse>
2008-01-15 14:33:44 +0000

    Dokumentacja dla wyjątków

--- Exceptions.py
+++ Exceptions.py
@@ -6,19 +6,37 @@
 # $Id$
 
 class GGException(Exception):
+	"""
+	Wyjatej "typu" gadu-gadu.
+	"""
 	pass
 
 class GGUnexceptedPacket(GGException):
+	"""
+	Wyjatek gadu-gadu dla nieoczekiwanego pakietu.
+	"""
 	pass
 	
 class GGNotLogged(GGException):
+	"""
+	Wyjatek gadu-gadu - niezalogowany uzytkownik.
+	"""
 	pass
 
 class GGServerNotOperating(GGException):
+	"""
+	Wyjatek gadu-gadu. Serwer nie odpowiada.
+	"""
 	pass
 	
 class GGBadTokenVal(GGException):
+	"""
+	Wyjatek gadu-gadu. Bledny token.
+	"""
 	pass
 
-class GGNotInContactsList(GGException):
+class GGNotInContactsList(GGException):'
+	"""
+	Wyjatek gadu-gadu. Nie ma uzytkownika w liscie kontaktow.
+	"""
 	pass
